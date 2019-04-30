afcceb2cbfa4f44fd8853ab6365d081ccdad8ef6
jojus1101 <jojus1101@10.50.131.75>
2019-04-30 14:07:01 +0200

    Merge origin/master Added carport class info

--- src/main/java/Data/Model_Carport.java
+++ src/main/java/Data/Model_Carport.java
@@ -10,10 +10,50 @@ package Data;
  * @author stein
  */
 public class Model_Carport {
+        private final int height;
+    private final int length;
+    private final int width; 
+    private final boolean shed;
+    private final String details;
+
+    public Model_Carport(int height, int length, int width, boolean shed, String details) {
+        this.height = height;
+        this.length = length;
+        this.width = width;
+        this.shed = shed;
+        this.details = details;
+    }
     
+    public Model_Carport(int height, int length, int width) {
+        this.height = height;
+        this.length = length;
+        this.width = width;
+        shed = false;
+        details = "";
+    }
+
+    public int getHeight() {
+        return height;
+    }
     
-    public Model_Carport()
-    {
-        
+    public int getLength() {
+        return length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+    
+    public boolean hasToolshed() {
+        return shed;
+    }
+    
+    public String getDetails() {
+        return details;
+    }
+
+    @Override
+    public String toString() {
+        return "Model_Carport{" + "height=" + height + ", length=" + length + ", width=" + width + ", shed=" + shed + ", details=" + details + '}';
     }
 }
