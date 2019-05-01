4d7440f6a3493bf9d4cc0a38662a65480f99f061
Noell <cph-ng67@cphbusiness.dk>
2019-05-01 12:50:40 +0200

    Removed height

--- src/main/java/Data/Model_Carport.java
+++ src/main/java/Data/Model_Carport.java
@@ -10,31 +10,29 @@ package Data;
  * @author stein
  */
 public class Model_Carport {
-        private final int height;
+       
     private final int length;
     private final int width; 
     private final boolean shed;
     private final String details;
 
-    public Model_Carport(int height, int length, int width, boolean shed, String details) {
-        this.height = height;
+    public Model_Carport(int length, int width, boolean shed, String details) {
+      
         this.length = length;
         this.width = width;
         this.shed = shed;
         this.details = details;
     }
     
-    public Model_Carport(int height, int length, int width) {
-        this.height = height;
+    public Model_Carport( int length, int width) {
+        
         this.length = length;
         this.width = width;
         shed = false;
         details = "";
     }
 
-    public int getHeight() {
-        return height;
-    }
+ 
     
     public int getLength() {
         return length;
@@ -54,6 +52,6 @@ public class Model_Carport {
 
     @Override
     public String toString() {
-        return "Model_Carport{" + "height=" + height + ", length=" + length + ", width=" + width + ", shed=" + shed + ", details=" + details + '}';
+        return "Model_Carport{"  + ", length=" + length + ", width=" + width + ", shed=" + shed + ", details=" + details + '}';
     }
 }
