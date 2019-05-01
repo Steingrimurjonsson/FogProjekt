194bb55be5adb4ce3de0ac55721e4f76ea139878
jojus1101 <jojus1101@10.50.131.75>
2019-05-01 13:22:22 +0200

    Merge origin/master Added carport class info more, and Mapper

--- src/main/java/Data/Model_Carport.java
+++ src/main/java/Data/Model_Carport.java
@@ -14,16 +14,20 @@ public class Model_Carport {
     private final int width;
     private final String material;
     private final boolean shed;
+    private final int shedwidth;
+    private final int shedlength;
     private final String details;
 
-    public Model_Carport(int length, int width,String material , boolean shed, String details) {
+    public Model_Carport(int length, int width, String material, boolean shed, int shedwidth, int shedlength, String details) {
         this.length = length;
         this.width = width;
+        this.material = material;
         this.shed = shed;
+        this.shedwidth = shedwidth;
+        this.shedlength = shedlength;
         this.details = details;
-        this.material= material;
     }
-    
+
     public int getLength() {
         return length;
     }
@@ -31,22 +35,33 @@ public class Model_Carport {
     public int getWidth() {
         return width;
     }
-    
-    public boolean hasShed() {
-        return shed;
-    }
-    
-    public String getDetails() {
-        return details;
-    }
 
     public String getMaterial() {
         return material;
     }
-    
+
+    public boolean isShed() {
+        return shed;
+    }
+
+    public int getShedwidth() {
+        return shedwidth;
+    }
+
+    public int getShedlength() {
+        return shedlength;
+    }
+
+    public String getDetails() {
+        return details;
+    }
 
     @Override
     public String toString() {
-        return "Model_Carport{" + ", length=" + length + ", width=" + width + ", shed=" + shed + ", details=" + details + '}';
+        return "Model_Carport{" + "length=" + length + ", width=" + width + ", material=" + material + ", shed=" + shed + ", shedwidth=" + shedwidth + ", shedlength=" + shedlength + ", details=" + details + '}';
     }
+
+    
+
+    
 }
