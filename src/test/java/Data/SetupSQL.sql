895085b318cb13da45f4a7b1af3b26ab63355252
Artin <artin-4000@hotmail.com>
2019-05-12 18:57:40 +0200

    changes in setup.sql

--- src/test/java/Data/SetupSQL.sql
+++ src/test/java/Data/SetupSQL.sql
@@ -11,15 +11,13 @@
 -- There is a schema named "useradmin" with one table: "Users"
 -- The test schema is named "useradminTest"
 -- make a copy of the table
-CREATE TABLE useradminTest.Users LIKE useradmin.Users;
--- make an other copy, this time naming it UsersTest
-CREATE TABLE UsersTest LIKE useradminTest.Users;
+CREATE TABLE FOG.UsersTest LIKE FOG.user;
 INSERT INTO `UsersTest` VALUES 
-    (1,'jens@somewhere.com','jensen','customer'),
-    (2,'ken@somewhere.com','kensen','customer'),
-    (3,'robin@somewhere.com','batman','employee'),
-    (4,'someone@nowhere.com','sesam','customer');
+    (1,'jens@somewhere.com','jensen','customer','customer','customer','customer','customer','customer','customer','customer'),
+    (2,'ken@somewhere.com','kensen','customer','customer','customer','customer','customer','customer','customer','customer'),
+    (3,'robin@somewhere.com','batman','employee','customer','customer','customer','customer','customer','customer','customer'),
+    (4,'someone@nowhere.com','sesam','customer','customer','customer','customer','customer','customer','customer','customer');
 
 -- make a new user for the test database
-CREATE USER 'testinguser' IDENTIFIED BY 'try1try2tryAgain';
-GRANT ALL PRIVILEGES ON useradminTest.* TO 'testinguser';
+CREATE USER 'admin' IDENTIFIED BY 'admin2019';
+GRANT ALL PRIVILEGES ON useradminTest.* TO 'admin';
