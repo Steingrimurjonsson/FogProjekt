dfafe927cf91e342dccc777a921386548fa6ae76
Noell Zane <cph-ng67@cphbusiness.dk>
2019-05-13 17:47:50 +0200

    Draft for order

--- src/main/java/Data/OrderMapper.java
+++ src/main/java/Data/OrderMapper.java
@@ -47,5 +47,34 @@ public class OrderMapper {
         }
       
     }
+      //This is not ready yet, it's just a draft of what orderlist COULD look like. Will edit once we decide on order structure
     
+     /* public static List<Order> allOrders() throws Exception
+    {
+        List<Order> list = new ArrayList();
+        try
+        {
+            Connection con = Connector.connection();
+            String SQL = "SELECT * from `order`";
+            PreparedStatement ps = con.prepareStatement(SQL);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next())
+            {
+                int  = rs.getInt("idOrder");
+                int idUser = rs.getInt("idUser");
+                int length = rs.getInt("length");
+                int width = rs.getInt("width");
+                int height = rs.getInt("height");
+                int material = rs.getInt("idMaterial")
+                boolean shed = rs.getBoolean("shed");
+                int shedWidth = rs.getInt("shedWidth")
+                int shedLength = rs.getInt("shedLength")
+                String details = rs.getString("details")
+                int roofslope = rs.getInt("slope")
+              
+
+                Order o = new Carport(idOrder,idUser, length, width, height,material, shed, shedWidth, shedLength, details, roofslope)
+                list.add(o);
+            }*/ 
 }
