113c7770e3ac807a3956bb975710a0b66382feb8
Chris <Stofferdyr@gmail.com>
2019-05-14 13:09:30 +0200

    more usermapper test

--- src/test/java/Data/Mapper_UserTest.java
+++ src/test/java/Data/Mapper_UserTest.java
@@ -21,68 +21,70 @@ import static org.junit.Assert.*;
  */
 public class Mapper_UserTest {
 
-    private static Connection singleton;
-    private static final String URL = "jdbc:mysql://167.99.209.146:3306/FOG?UseSSL=false";
-    private static final String USERNAME = "admin";
-    private static final String PASSWORD = "admin2019";
-
-    public static void setConnection(Connection con) {
-        singleton = con;
-    }
+  private static Connection testConnection;
+    private static String USER = "testinguser";
+    private static String USERPW = "try1try2tryAgain";
+    private static String DBNAME = "useradminTest";
+    private static String HOST = "167.99.209.146";
 
     @Before
     public void setUp() {
         try {
             // awoid making a new connection for each test
-            if (singleton == null) {
+            if ( testConnection == null ) {
+                String url = String.format( "jdbc:mysql://%s:3306/%s", HOST, DBNAME );
+                Class.forName( "com.mysql.cj.jdbc.Driver" );
 
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                singleton = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                testConnection = DriverManager.getConnection( url, USER, USERPW );
                 // Make mappers use test 
-                Connector.setConnection(singleton);
+                Connector.setConnection( testConnection );
             }
             // reset test database
-            try (Statement stmt = singleton.createStatement()) {
-                stmt.execute("drop table if exists Users");
-                stmt.execute("create table Users like UsersTest");
-                stmt.execute("insert into Users select * from UsersTest");
+            try ( Statement stmt = testConnection.createStatement() ) {
+                stmt.execute( "drop table if exists Users" );
+                stmt.execute( "create table Users like UsersTest" );
+                stmt.execute( "insert into Users select * from UsersTest" );
             }
 
-        } catch (ClassNotFoundException | SQLException ex) {
-            singleton = null;
-            System.out.println("Could not open connection to database: " + ex.getMessage());
+        } catch ( ClassNotFoundException | SQLException ex ) {
+            testConnection = null;
+            System.out.println( "Could not open connection to database: " + ex.getMessage() );
         }
     }
 
     @Test
     public void testSetUpOK() {
         // Just check that we have a connection.
-        assertNotNull(singleton);
+        assertNotNull( testConnection );
     }
 
     @Test
     public void testLogin01() throws LoginSampleException {
         // Can we log in
-        User user = Mapper_User.login("jens@somewhere.com", "jensen");
-        assertTrue(user != null);
+        User user = Mapper_User.login( "jens@somewhere.com", "jensen" );
+        assertTrue( user != null );
+    }
+
+    @Test( expected = LoginSampleException.class )
+    public void testLogin02() throws LoginSampleException {
+        // We should get an exception if we use the wrong password
+        User user = Mapper_User.login( "jens@somewhere.com", "larsen" );
     }
 
     @Test
     public void testLogin03() throws LoginSampleException {
         // Jens is supposed to be a customer
-        User user = Mapper_User.login("jens@somewhere.com", "jensen");
-        assertEquals("customer", user.getRole());
+        User user = Mapper_User.login( "jens@somewhere.com", "jensen" );
+        assertEquals( "customer", user.getRole() );
     }
 
-    @Test //spørg Anders
+    @Test
     public void testCreateUser01() throws LoginSampleException {
         // Can we create a new user - Notice, if login fails, this will fail
         // but so would login01, so this is OK
         User original = new User("king@kong.com", "uhahvorhemmeligt", "konge", "", "", "", "", "", "", "");
         Mapper_User.createUser(original);
-        User retrieved = Mapper_User.login("king@kong.com", "uhahvorhemmeligt");
-        assertEquals("konge", retrieved.getRole());
-
+        User retrieved = Mapper_User.login( "king@kong.com", "uhahvorhemmeligt" );
+        assertEquals( "konge", retrieved.getRole() );
     }
-
 }
