c07c903ecec3eb3c9fde9150d1ce8a601d320753
Stein <steingrimurkj@gmail.com>
2019-05-16 15:43:20 +0200

    Update Material.java

--- src/main/java/Data/Material.java
+++ src/main/java/Data/Material.java
@@ -11,32 +11,41 @@ package Data;
  */
 public class Material
 {
-    private final int id;
-    private final String material;
-    private final double price;
+    private int id;
+    private String material;
+    private int stock;
+    
+   
 
-    public Material(int id, String material, double price)
+    public Material(int id, String material, int stock)
     {
         this.id = id;
         this.material = material;
-        this.price = price;
+        this.stock = stock;
     }
 
-    public int getId()
-    {
+    public int getId() {
         return id;
     }
 
-    public String getMaterial()
-    {
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMaterial() {
         return material;
     }
 
-    public double getPrice()
-    {
-        return price;
+    public void setMaterial(String material) {
+        this.material = material;
     }
-    
-    
-    
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
 }
