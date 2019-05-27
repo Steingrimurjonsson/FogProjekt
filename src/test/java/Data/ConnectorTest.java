74d14aa322e4c966cd183b129aa05737c135f782
Noell Zane <cph-ng67@cphbusiness.dk>
2019-05-28 00:05:17 +0200

    Added test for connector

---
+++ src/test/java/Data/ConnectorTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sus
+ */
+public class ConnectorTest {
+    
+   @Test
+    public void testGetConnection() throws  ClassNotFoundException, SQLException {
+        Connector singleton = new Connector();
+        Connection con = singleton.connection();
+        boolean expResult = true;
+        boolean result = con != null;
+        assertEquals(expResult, result);
+    }
+}
