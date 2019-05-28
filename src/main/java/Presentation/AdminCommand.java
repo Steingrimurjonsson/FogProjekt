959633cbf0b0e660afd844db011970b66216be35
DESKTOP-9JUPJQB\stein <steingrimurkj@gmail.com>
2019-05-29 00:35:26 +0200

    Update src/main/java/Presentation/AdminCommand.java

--- src/main/java/Presentation/AdminCommand.java
+++ src/main/java/Presentation/AdminCommand.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package Presentation;
-
 import Logic.Models.Model_Stock;
 import Logic.LogicFacade;
 import Logic.Models.Model_Order;
@@ -13,27 +12,21 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 /**
- *
  * @author NoellZane
  */
 public class AdminCommand extends Command {
-
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         try {
             HttpSession session = request.getSession();
-
             if (request.getParameter("stock") != null) {
                 String matIdText = request.getParameter("matID");
                 String stockText = request.getParameter("stock");
                 int matID = Integer.parseInt(matIdText);
                 int stock = Integer.parseInt(stockText);
                 LogicFacade.addStockById(matID, stock);
-
             }
-
             List<Model_Stock> stockList = LogicFacade.getStock();
             List<Model_User> userList = LogicFacade.allUsers();
             List<Model_Order> orderList = LogicFacade.getAllOrders();
