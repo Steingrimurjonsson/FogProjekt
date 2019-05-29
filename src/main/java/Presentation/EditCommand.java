1d98ec092e987f08211939431b20c9c75d5457d2
DESKTOP-9JUPJQB\stein <steingrimurkj@gmail.com>
2019-05-29 11:17:02 +0200

    Update src/main/java/Presentation/EditCommand.java

--- src/main/java/Presentation/EditCommand.java
+++ src/main/java/Presentation/EditCommand.java
@@ -3,6 +3,7 @@ package Presentation;
 import Logic.LogicFacade;
 import Logic.Exceptions.CustomerException;
 import Logic.Models.Model_User;
+import java.nio.charset.StandardCharsets;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -11,28 +12,34 @@ public class EditCommand extends Command {
 
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws CustomerException {
-
-        String email = request.getParameter("email");
-        String password1 = request.getParameter("password1");
-        String password2 = request.getParameter("password2");
-        String firstName = request.getParameter("fname");
-        String lastName = request.getParameter("lname");
-        String phone = request.getParameter("phone");
-        String street = request.getParameter("street");
-        String city = request.getParameter("city");
-        String zip = request.getParameter("zip");
-        String country = request.getParameter("country");
-
+        //Changes the user information and makes it the right charset format
+            String email = request.getParameter("email");
+            byte[] bytes = email.getBytes(StandardCharsets.ISO_8859_1);
+            email = new String(bytes, StandardCharsets.UTF_8);
+            String password1 = request.getParameter("password1");
+            String password2 = request.getParameter("password2");
+            String firstName = request.getParameter("fname");
+            byte[] FNbytes = firstName.getBytes(StandardCharsets.ISO_8859_1);
+            firstName = new String(FNbytes, StandardCharsets.UTF_8);
+            String lastName = request.getParameter("lname");
+            byte[] LNbytes = lastName.getBytes(StandardCharsets.ISO_8859_1);
+            lastName = new String(LNbytes, StandardCharsets.UTF_8);
+            String phone = request.getParameter("phone");
+            String street = request.getParameter("street");
+            byte[] Sbytes = street.getBytes(StandardCharsets.ISO_8859_1);
+            street = new String(Sbytes, StandardCharsets.UTF_8);
+            String city = request.getParameter("city");
+            byte[] Cbytes = city.getBytes(StandardCharsets.ISO_8859_1);
+            city = new String(Cbytes, StandardCharsets.UTF_8);
+            String zip = request.getParameter("zip");
+            String country = request.getParameter("country");
+            byte[] CObytes = country.getBytes(StandardCharsets.ISO_8859_1);
+            country = new String(CObytes, StandardCharsets.UTF_8);
         HttpSession session = request.getSession();
         Model_User oldUserData = (Model_User) session.getAttribute("user");
-
         if (password1.equals(password2)) {
             Model_User user = LogicFacade.editUser(oldUserData.getId(), email, password1, firstName, lastName, phone, street, city, zip, country);
-
             session.setAttribute("user", user);
-
-            // session.setAttribute( "role", user.getRole() );
-            // return user.getRole() + "page";
         }
         return "customerpage";
     }
